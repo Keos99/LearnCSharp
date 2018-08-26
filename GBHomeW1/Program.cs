8d0145b217af0f9df39d2791d70dfafda26545c4
Keos99 <keos99@bk.ru>
2018-08-27 00:02:31 +0300

    Добавлено первое задание первого урока. Added the first task of the first lesson/

---
+++ GBHomeW1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GBHomeW_1
+{
+    class Program
+    {
+        int firstnumber = 0;
+        int secondnumber = 0;
+        int sum = 0;
+        string choosemenu = null;
+        string stringnumber = null;          
+
+        public void menu()
+        {
+            Console.WriteLine("Выберите нужную операцию: ");
+            Console.WriteLine("\t1) Сложение двух целых чисел.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+
+        void chooseMenu()
+        {
+            choosemenu = Console.ReadLine();
+            switch (choosemenu)
+            {
+                
+            }
+        }
+
+        void twoIntSumm()
+        {
+            Console.WriteLine("Введите первое число: ");
+            stringnumber = Console.ReadLine();
+            firstnumber = Convert.ToInt32(stringnumber);
+            Console.WriteLine("Введите второе число: ");
+            stringnumber = Console.ReadLine();            
+            secondnumber = Convert.ToInt32(stringnumber);
+            sum = firstnumber + secondnumber;
+            Console.WriteLine("Сумма " + firstnumber + " и " + secondnumber + " равно " + sum);
+        }       
+
+        static void Main(string[] args)
+        {          
+            while (true)
+            {
+
+            }
+
+            Console.WriteLine("Приветствуем в приложении по сложению двух чисел!");
+                        
+            
+        }
+    }
+}
